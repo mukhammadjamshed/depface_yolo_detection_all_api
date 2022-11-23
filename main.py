496ed397efbeaf07d12eefea0db6e0a45a6ea842
Khudayberdiyev <100419155+KaaPlya@users.noreply.github.com>
2022-11-23 11:02:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -60,31 +60,30 @@ async def create_files(file: UploadFile=File(...)):
     print(file.filename, filename)
 
     net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-    #save all the names in file o the list classes
+    
     classes = []
     with open("coco.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
-    #get layers of the network
+    
     layer_names = net.getLayerNames()
-    #Determine the output layer names from the YOLO model 
+    
     output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
-    # print("YOLO LOADED")
+  
 
 
     #---------------------------------------------------------------#
-     # Capture frame-by-frame
+    
     img = cv2.imread(filename)
     #     img = cv2.resize(img, None, fx=0.4, fy=0.4)
     height, width, channels = img.shape
 
-        # USing blob function of opencv to preprocess image
+       
     blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416),
          swapRB=True, crop=False)
-        #Detecting objects
     net.setInput(blob)
     outs = net.forward(output_layers)
 
-        # Showing informations on the screen
+        
     class_ids = []
     confidences = []
     boxes = []
@@ -94,13 +93,13 @@ async def create_files(file: UploadFile=File(...)):
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.5:
-                    # Object detected
+                    
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
 
-                    # Rectangle coordinates
+                    
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
 
@@ -109,8 +108,7 @@ async def create_files(file: UploadFile=File(...)):
                 class_ids.append(class_id)
 
     labels_list= []    
-    #We use NMS function in opencv to perform Non-maximum Suppression
-    #we give it score threshold and nms threshold as arguments.
+   
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
     for i in range(len(boxes)):
@@ -130,8 +128,7 @@ async def create_files(file: UploadFile=File(...)):
     print(count_elements)
 
         
-    # cv2.imshow("Image",img)
-    # cv2.waitKey(0)
+
 
 
     return {"Count of elements": count_elements}
